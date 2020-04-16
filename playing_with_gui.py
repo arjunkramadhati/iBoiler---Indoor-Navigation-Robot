f388205e69f92dec2ed2c581aa746a98bf696d68
Heejae Han <heejae@Heejaes-MacBook-Pro.local>
2020-04-16 19:06:19 -0400

    April 16

---
+++ playing_with_gui.py
@@ -0,0 +1,10 @@
+import guibuilder as gui
+
+'''
+Place your map image file in the current directory you are running pythons codes.
+(i.e. if your pythons codes are placed in C:/Users/Desktop/CNIT581/Project, 
+place the image file in the exact same directory. 
+You can call the gui window by passing map image as an argument.
+'''
+
+gui.Window(mapImage="map.jpg")
