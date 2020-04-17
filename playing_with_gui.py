e71d5c113ede991f9e11d420e37039dc0909ed96
Heejae Han <heejae@Heejaes-MacBook-Pro.local>
2020-04-16 21:49:46 -0400

    Update April 16

--- playing_with_gui.py
+++ playing_with_gui.py
@@ -1,9 +1,13 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import tkinter.font as font
 import guibuilder as gui
 
 '''
-Place your map image file in the current directory you are running pythons codes.
+1. Import all the necessary libraries. (Have all the libraries above.)
+2. Place your map image file in the current directory you are running pythons codes.
 (i.e. if your pythons codes are placed in C:/Users/Desktop/CNIT581/Project, 
-place the image file in the exact same directory. 
+place the image file in the exact same directory.)
 You can call the gui window by passing map image as an argument.
 '''
 
